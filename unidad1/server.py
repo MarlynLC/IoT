02aabb65e403f046847dfc33d6045235dfe7e218
MarlynLC <18460706@colima.tecnm.mx>
2023-09-07 16:51:10 -0600

    test

--- unidad1/server.py
+++ unidad1/server.py
@@ -1,8 +1,11 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
+#inicializamos el contador en 10
+
 contador = 10
 
+#response establece la respuesta HTTP el código 200 éxito
 class MyHTTPRequestHandler(BaseHTTPRequestHandler):
     def _set_response(self, content_type="text/plain"):
         self.send_response(200)
@@ -43,6 +46,9 @@ class MyHTTPRequestHandler(BaseHTTPRequestHandler):
         response_data = json.dumps({"message": "Received POST data", "data": post_data.decode()})
         self._set_response("application/json")
         self.wfile.write(response_data.encode())
+
+
+#conexión con visual studio code y postman 
 
 def run_server(server_class=HTTPServer, handler_class=MyHTTPRequestHandler, port=7800):
     server_address = ("", port)
